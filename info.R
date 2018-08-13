2384847675e4e26265241b420bbe4f6d13a7ddc1
Sanya Sharma <36171634+sanyabd13@users.noreply.github.com>
2018-08-13 14:19:31 -0400

    Delete info.R

--- info.R
+++
@@ -1,5 +0,0 @@
-install.packages('rsconnect')
-rsconnect::setAccountInfo(name='sanyabd13',
-                          token='AB50333550C70D38EF323CF5D834D3D4',
-                          secret='7FqWqrFZuMzvRmOb75dt7zU6fQKaEc5wJcPQiZE0')
-runApp()
