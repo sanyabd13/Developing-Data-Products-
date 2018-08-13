c8b8d6a9b3c435c5070ff5b6ea9c7dd797779943
Sanya Sharma <36171634+sanyabd13@users.noreply.github.com>
2018-08-13 14:19:03 -0400

    Add files via upload

---
+++ info.R
@@ -0,0 +1,5 @@
+install.packages('rsconnect')
+rsconnect::setAccountInfo(name='sanyabd13',
+                          token='AB50333550C70D38EF323CF5D834D3D4',
+                          secret='7FqWqrFZuMzvRmOb75dt7zU6fQKaEc5wJcPQiZE0')
+runApp()
